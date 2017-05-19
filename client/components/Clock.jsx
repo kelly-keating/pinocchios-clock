d9b94437e2f4c23639a672a047a22e08ba8bf9ad
bill-burgess <wmhburgess@gmail.com>
2017-05-19 15:46:51 +1200

    time counting

--- client/components/Clock.jsx
+++ client/components/Clock.jsx
@@ -5,16 +5,24 @@ class Clock extends React.Component {
 
   constructor (props) {
     super(props)
-
+    this.state = {
+      h: 0,
+      m: 0,
+      s: 0
+    }
   }
 
   componentDidMount(){
-    setInterval(this.recolour, 50)
-    console.log(this.props.location.pathname);
+    setInterval(() => {this.recolour()}, 50)
   }
 
   recolour() {
     var date = new Date()
+    this.setState({
+      h: date.getHours(),
+      m: date.getMinutes(),
+      s: date.getSeconds()
+    })
     var s = Math.floor(date.getSeconds()*255/59)
     var m = Math.floor(date.getMinutes()*255/59)
     var h = Math.floor(date.getHours()*255/23)
@@ -29,11 +37,16 @@ class Clock extends React.Component {
   }
 
   ownPage() {
+    var date = new Date()
+    var s = date.getSeconds()
+    var m = date.getMinutes()
+    var h = date.getHours()
+
     return(
-      <div id='clock'>TIME
-        <div id='time'>
-          <Link to='/'>BACK IN</Link>
-        </div>
+      <div id='clock'>
+        TIME
+        <div id='time'>#{h}:{m}:{s}</div>
+        <Link to='/'>BACK IN</Link>
       </div>
     )
   }
